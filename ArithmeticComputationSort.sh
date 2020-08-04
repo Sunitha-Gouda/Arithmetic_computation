ff94ac0c21e41569a5e885f463730bebaec70ec9
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-04 11:29:36 +0530

    Compute of expression1

--- ArithmeticComputationSort.sh
+++ ArithmeticComputationSort.sh
@@ -2,7 +2,9 @@
 
 echo " Arithmetic computation stimulation "
 
-read -p "Enter the value  of a = " val_a
-read -p "Enter the value  of b = " val_b
-read -p "Enter the value  of c = " val_c
+read -p "Enter the value  of a = " vara
+read -p "Enter the value  of b = " varb
+read -p "Enter the value  of c = " varc
 
+exp1=($vara+$varb*$varc)
+	echo " a+b*c = " $exp1
