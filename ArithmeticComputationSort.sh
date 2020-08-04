17118a6ce17bb6cd830f03306e72d4f0bcb48e9a
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-04 16:12:15 +0530

    Store results in Dictionary

--- ArithmeticComputationSort.sh
+++ ArithmeticComputationSort.sh
@@ -1,16 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo " Arithmetic computation stimulation "
+
+declare -A sorting
 
 read -p "Enter the value  of a = " vara
 read -p "Enter the value  of b = " varb
 read -p "Enter the value  of c = " varc
 
-exp1=($vara+$varb*$varc)
+exp1=$(($vara+$varb*$varc))
 	echo " a+b*c = " $exp1
-exp2=($vara*$varb+$varc)
+
+exp2=$(($vara*$varb+$varc))
    echo " a*b+c = " $exp2
-exp3=($varc+$vara/$varb)
+
+exp3=$(($varc+$vara/$varb))
    echo " a+b/c = " $exp3
-exp4=($vara%$varb+$varc)
+
+exp4=$(($vara%$varb+$varc))
    echo " a%b+c = " $exp4
+
+
+sorting=([0]=$exp1 [1]=$exp2 [2]=$exp3 [3]=$exp4)
+	echo "${sorting[@]}"
