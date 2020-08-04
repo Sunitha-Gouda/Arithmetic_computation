e7c423555aec52824bc9b012955a4356bd36ca12
Sunitha-Gouda <69080731+Sunitha-Gouda@users.noreply.github.com>
2020-08-04 12:35:00 +0530

    Update ArithmeticComputationSort.sh

--- ArithmeticComputationSort.sh
+++ ArithmeticComputationSort.sh
@@ -6,9 +6,9 @@ read -p "Enter the value  of a = " vara
 read -p "Enter the value  of b = " varb
 read -p "Enter the value  of c = " varc
 
-exp1=($vara+$varb*$varc)
+exp1=$(($vara+$varb*$varc))
 	echo " a+b*c = " $exp1
-exp2=($vara*$varb+$varc)
+exp2=$(($vara*$varb+$varc))
    echo " a*b+c = " $exp2
-exp3=($varc+$vara/$varb)
+exp3=$(($varc+$vara/$varb))
    echo " a+b/c = " $exp3
