05e565b846b869892e6ee9db1e31c459fedfc71d
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-04 11:18:31 +0530

    Heading added

--- ArithmeticComputationSort.sh
+++ ArithmeticComputationSort.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo " Arithmetic computation stimulation "
+
 read -p "Enter the value  of a = " val_a
 read -p "Enter the value  of b = " val_b
 read -p "Enter the value  of c = " val_c
